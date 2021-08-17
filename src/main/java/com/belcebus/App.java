d65a6d05b387942112c2d82a860706ead23a413e
David Hernández Martín <belcebus@gmail.com>
2021-08-17 19:39:26 +0200

    fixing

--- src/main/java/com/belcebus/App.java
+++ src/main/java/com/belcebus/App.java
@@ -1,4 +1,4 @@
-package com.theironbank;
+package com.belcebus;
 
 public class App 
 {
--- src/test/java/com/belcebus/AppTest.java
+++ src/test/java/com/belcebus/AppTest.java
@@ -1,4 +1,4 @@
-package com.theironbank;
+package com.belcebus;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
